ba53b3794d1b46b2cea1944c7e93c2a1cce75cb3
Jeet Jadav <jeetjadav11@gmail.com>
2025-10-02 12:19:57 -0400

    did till 37 but ithink i did some questions worng

---
+++ __SHELL13.java
@@ -0,0 +1,10 @@
+
+public class __SHELL13 extends bluej.runtime.Shell {
+public static void run() throws Throwable {
+final bluej.runtime.BJMap __bluej_runtime_scope = getScope("C:\\Users\\harsh\\Downloads\\projects\\bluej-projects\\Chapter06\\tech-support-complete");
+final SupportSystem supportS1 = (SupportSystem)__bluej_runtime_scope.get("supportS1");
+
+
+supportS1.start();
+
+}}
